20ef694035fe292f09d509eb1063483dee8bb211
Gourav Chatterjee <chatterjeegouravking@gmail.com>
2020-02-10 21:15:54 +0530

    CHANGES

--- CONTENT/ROOT_URI/signIn.php
+++ CONTENT/ROOT_URI/signIn.php
@@ -57,20 +57,8 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
+      
       <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="noPassword" class="text-center">I don't have a password</a>
-      </p>
       <p class="mb-1">
         <a href="forgotpassword">I forgot my password</a>
       </p>
